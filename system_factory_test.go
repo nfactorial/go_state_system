a1dd5b0a39b9c1226adbd6ef5268c71e05bbf2f7
nfactorial <nfactorial@btopenworld.com>
2017-06-16 14:31:24 +0100

    Updates system factory unit tests to verify behaviour of Create method.

--- system_factory_test.go
+++ system_factory_test.go
@@ -3,6 +3,7 @@ package state_system
 import "testing"
 
 const TEST_FACTORY_SYSTEM_NAME = "TestFactory"
+const TEST_INVALID_SYSTEM_NAME = "InvalidSystem"
 
 type FactoryTestMockGameSystem struct {
 	counter int
@@ -13,11 +14,10 @@ func (system *FactoryTestMockGameSystem) OnDestroy() {
 }
 
 func (system *FactoryTestMockGameSystem) OnInitialize(initArgs *InitArgs) {
-	system.counter++
 }
 
 func (system *FactoryTestMockGameSystem) OnActivate() {
-
+	system.counter++
 }
 
 func (system *FactoryTestMockGameSystem) OnDeactivate() {
@@ -65,5 +65,37 @@ func TestSystemFactory_Unregister(t *testing.T) {
 }
 
 func TestSystemFactory_Create(t *testing.T) {
+	factory := CreateSystemFactory()
 
+	system := factory.Create(TEST_FACTORY_SYSTEM_NAME)
+	if system != nil {
+		t.Error("System factory successfully created object before it was registered.")
+	}
+
+	factory.Register(TEST_FACTORY_SYSTEM_NAME, MockGameSystemConstructor)
+
+	system = factory.Create(TEST_INVALID_SYSTEM_NAME)
+	if system != nil {
+		t.Error("Expected invalid system name creation to fail, but it succeeded.")
+	}
+
+	system = factory.Create(TEST_FACTORY_SYSTEM_NAME)
+	if system == nil {
+		t.Error("Failed to create factory object after registration.")
+	}
+
+	testInstance, ok := system.(*FactoryTestMockGameSystem)
+	if !ok {
+		t.Error("Returned object from factory was not of expected type.")
+	}
+
+	if testInstance.counter != 0 {
+		t.Error("Created instance contained an invalid counter value.")
+	}
+	// Invoke our counter method, so we can check they are the same object
+	system.OnActivate()
+
+	if testInstance.counter != 1 {
+		t.Error("Counter was an unexpected value, expected 1 but found", testInstance.counter)
+	}
 }
